dc7a39864913249a4e7e6bc0e30fb7e52e6527cd
Maxim Datskovskiy <mdatskovsky@mirantis.com>
2014-06-27 18:27:39 +0300

    fix neutron ha

--- deployment/puppet/openstack/manifests/ha/neutron.pp
+++ deployment/puppet/openstack/manifests/ha/neutron.pp
@@ -1,6 +1,8 @@
 # HA configuration for OpenStack Neutron
 class openstack::ha::neutron {
 
+  include 'neutron::waist_setup'
+
   openstack::ha::haproxy_service { 'neutron':
     order           => '085',
     listen_port     => 9696,
@@ -8,6 +10,9 @@ class openstack::ha::neutron {
     define_backups  => true,
     require_service => 'neutron-server',
   }
+
+  Class['neutron'] -> Class['neutron::waistline']
+  anchor {'neutron-api-up':}
 
   Openstack::Ha::Haproxy_service<|title == 'keystone-1' or title == 'keystone-2'|> -> Service['neutron-server']
   Openstack::Ha::Haproxy_service['neutron'] -> Class['neutron::waistline']
